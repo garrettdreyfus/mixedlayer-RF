d882ae2fc40f33501a492fc38727f244b2a13e24
Garrett Finucane <garrettdreyfus@gmail.com>
2021-11-09 09:24:38 -0800

    nice nice nice

--- diagnose.py
+++ diagnose.py
@@ -6,8 +6,9 @@ from os.path import basename
 from scipy import interpolate
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from holteandtalley import HolteAndTalley
+from sklearn.preprocessing import StandardScaler
 matplotlib.use('pdf')
 
 
@@ -45,7 +46,7 @@ with open('../MLDIdentifierTool/json_generator/profiles.json') as f:
     lon = []
     densities = [] 
     y = [ ]
-    chosenprofiles = np.random.choice(len(profiles), size=int(len(profiles)*0.25), replace=False)
+    chosenprofiles = np.random.choice(len(profiles), size=int(len(profiles)*0.5), replace=False)
     mask = np.zeros_like(profiles,bool)
     mask[chosenprofiles] = True
     chosenprofiles = mask
@@ -53,27 +54,33 @@ with open('../MLDIdentifierTool/json_generator/profiles.json') as f:
         if profile["name"] in names.keys():
             plt.plot(profile["densities"],profile["pressures"])
             depths = names[profile["name"]]["depths"]
-            denschoose = []
-            for d in depths:
-                if d >0:
-                    denschoose.append(profile["densities"][profile["pressures"].index(d)])
-                else:
-                    denschoose.append(np.nan)
-            f = interpolate.interp1d(profile["pressures"],profile["densities"])
-            xnew = np.arange(20, 150, 1)
-            ynew = f(xnew)
-            featuredens = ynew
-            y= y+depths
-            lon=lon+[profile["lon"]]*len(denschoose)
-            lat=lat + [profile["lat"]]*len(denschoose)
-            for l in range(len(denschoose)):
-                densities.append(list(featuredens)+list(np.diff(featuredens)))
-            date = profile["date"]
-            doy += [int(date[5:7])*30 + int(date[8:10])] * len(denschoose)
+            if len(depths)>0:
+                depths = list([int(np.nanmean(depths))])
+                denschoose = []
+                #for d in depths:
+                    #if d >0:
+                        #denschoose.append(profile["densities"][profile["pressures"].index(d)])
+                    #else:
+                        #denschoose.append(np.nan)
+                denschoose=[0]
+                d = interpolate.interp1d(profile["pressures"],profile["densities"])
+                s = interpolate.interp1d(profile["pressures"],profile["salinities"])
+                t = interpolate.interp1d(profile["pressures"],profile["temperatures"])
+                xnew = np.arange(20, 150, 1)
+                densnew = d(xnew)
+                salnew = s(xnew)
+                tempnew = t(xnew)
+                y= y+depths
+                lon=lon+[profile["lon"]]*len(denschoose)
+                lat=lat + [profile["lat"]]*len(denschoose)
+                for l in range(len(denschoose)):
+                    densities.append(list(densnew)+list(np.diff(densnew)) + list(salnew)+list(np.diff(salnew))+list(tempnew)+list(np.diff(tempnew)))
+                date = profile["date"]
+                doy += [int(date[5:7])*30 + int(date[8:10])] * len(denschoose)
 
-            #plt.scatter(denschoose,depths)
-            #plt.savefig('./pics/{}.png'.format(basename(profile["name"])))
-            #plt.close()
+                #plt.scatter(denschoose,depths)
+                #plt.savefig('./pics/{}.png'.format(basename(profile["name"])))
+                #plt.close()
 
 doy =np.asarray(doy).reshape((len(doy),1))
 lon =np.asarray(lon).reshape((len(doy),1))
@@ -83,6 +90,7 @@ y = np.asarray(y)
 X = np.hstack((doy,lon,lat,densities))
 ## DECISION TREE
 dec_tree = DecisionTreeRegressor()
+scalerX = StandardScaler().fit(X)
 dec_tree.fit(X,y)
 #y_pred_dec_tree=dec_tree.predict(X)
 #print("RMSE: ",np.sqrt(np.mean((y_test-y_pred_dec_tree)**2)))
@@ -91,9 +99,10 @@ dec_tree.fit(X,y)
 ##################
 ## Random Forest
 ### REALLY GOOD FOR SOME REASON
-#regr = RandomForestRegressor(random_state=0, oob_score=True, max_depth=7)
+#regr = RandomForestRegressor(random_state=0, oob_score=True)
 #Next
-regr = RandomForestRegressor(random_state=0, oob_score=True)
+#regr = RandomForestRegressor(random_state=0, oob_score=True, min_samples_split=10)
+regr = GradientBoostingRegressor(random_state=0)
 regr.fit(X,y)
 #y_pred_random=regr.predict(X_test)
 #print("oob score", regr.oob_score_)
@@ -115,42 +124,50 @@ with open('../MLDIdentifierTool/json_generator/profiles.json') as f:
     for profile in np.asarray(profiles)[~chosenprofiles]:
         if profile["name"] in names.keys():
             depths = names[profile["name"]]["depths"]
-            denschoose = []
-            for d in depths:
-                if d >0:
-                    denschoose.append(profile["densities"][profile["pressures"].index(d)])
-                else:
-                    denschoose.append(np.nan)
-            f = interpolate.interp1d(profile["pressures"],profile["densities"])
-            h = HolteAndTalley(profile["pressures"],profile["temperatures"],profile["salinities"],profile["densities"])
-            xnew = np.arange(20, 150, 1)
-            ynew = f(xnew)
-            date = profile["date"]
-            doy = int(date[5:7])*30 + int(date[8:10])
-            X = np.asarray([[doy,profile["lon"],profile["lat"]]+list(ynew)+list(np.diff(ynew))])
+            print(depths)
+            print(len(depths))
+            if len(depths)>0:
+                depths = list([int(np.nanmean(depths))])
+                denschoose = [0]
+                #for d in depths:
+                    #if d >0:
+                        #denschoose.append(profile["densities"][profile["pressures"].index(d)])
+                    #else:
+                        #denschoose.append(np.nan)
+                d = interpolate.interp1d(profile["pressures"],profile["densities"])
+                s = interpolate.interp1d(profile["pressures"],profile["salinities"])
+                t = interpolate.interp1d(profile["pressures"],profile["temperatures"])
+                h = HolteAndTalley(profile["pressures"],profile["temperatures"],profile["salinities"],profile["densities"])
+                xnew = np.arange(20, 150, 1)
+                densinterp = d(xnew)
+                salnew = s(xnew)
+                tempnew = t(xnew)
+                date = profile["date"]
+                doy = int(date[5:7])*30 + int(date[8:10])
+                X = np.asarray([[doy,profile["lon"],profile["lat"]]+list(densinterp)+list(np.diff(densinterp)) +list(salnew)+list(np.diff(salnew)) +list(tempnew)+list(np.diff(tempnew))])
 
-            X = np.asarray(X)
-            dec_out = dec_tree.predict(X)[0]
-            regr_out = regr.predict(X)[0]
-            mask = np.asarray(profile["pressures"])<200
-            g = interpolate.interp1d(profile["pressures"],profile["temperatures"])
-            #plt.plot(np.asarray(profile["temperatures"])[mask],np.asarray(profile["pressures"])[mask],color="black")
-            #plt.scatter(denschoose,depths,color="red")
-            #plt.scatter(f(dec_out),dec_out,color="blue")
-            dec_error.append((regr_out-np.mean(depths))**2)
-            #plt.scatter(f(regr_out),regr_out,color="green")
-            ht_error.append((h.tempMLD-np.mean(depths))**2)
-            #plt.scatter(f(h.tempMLD),h.tempMLD,color="black")
-            obs_std.append(np.std(depths))
-            try:
-                #plt.scatter(f(h.temp.TTMLD),h.temp.TTMLD,color="orange")
-                crit_error.append((h.temp.TTMLD-np.mean(depths))**2)
-            except:
-                crit_error.append(np.nan)
-                print("out of range")
+                X = np.asarray(X)
+                dec_out = dec_tree.predict(X)[0]
+                regr_out = regr.predict(X)[0]
+                mask = np.asarray(profile["pressures"])<200
+                g = interpolate.interp1d(profile["pressures"],profile["temperatures"])
+                #plt.plot(np.asarray(profile["temperatures"])[mask],np.asarray(profile["pressures"])[mask],color="black")
+                #plt.scatter(denschoose,depths,color="red")
+                #plt.scatter(f(dec_out),dec_out,color="blue")
+                dec_error.append((regr_out-np.mean(depths))**2)
+                #plt.scatter(f(regr_out),regr_out,color="green")
+                ht_error.append((h.tempMLD-np.mean(depths))**2)
+                #plt.scatter(f(h.tempMLD),h.tempMLD,color="black")
+                obs_std.append(np.std(depths))
+                try:
+                    #plt.scatter(f(h.temp.TTMLD),h.temp.TTMLD,color="orange")
+                    crit_error.append((h.temp.TTMLD-np.mean(depths))**2)
+                except:
+                    crit_error.append(np.nan)
+                    print("out of range")
 
-            #plt.savefig('./pics/{}.png'.format(basename(profile["name"])))
-            plt.close()
+                #plt.savefig('./pics/{}.png'.format(basename(profile["name"])))
+                plt.close()
 dec_error = np.sqrt(np.asarray(dec_error))
 ht_error = np.sqrt(np.asarray(ht_error))
 crit_error = np.sqrt(np.asarray(crit_error))
